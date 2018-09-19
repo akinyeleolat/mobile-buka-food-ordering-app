5eb243c9168695cc385a642ff4fa263542810126
akinyeleolat <akinyeleolat2005@gmail.com>
2018-09-19 20:44:17 +0100

    [Chore #160547666] Eliminate Create OrderItem Endpoint

--- api/model/orderModel.js
+++ api/model/orderModel.js
@@ -4,17 +4,6 @@
  * @returns {object} order.
  */
 const fetchorder = () => {
-  // const order = [
-  //   {
-  //     id: 1, customerName: 'Adesola James', deliveryAddress: 'CA 50, Ambrose Street,Allen', Accepted: false, orderStatus: 'Not Accepted',
-  //   },
-  //   {
-  //     id: 2, customerName: 'Adekunle Felicia', deliveryAddress: 'CA 60, Ambrose Street,Allen', Accepted: false, orderStatus: 'Not Accepted',
-  //   },
-  //   {
-  //     id: 3, customerName: 'Adebisi Felicia', deliveryAddress: 'CA 60, Ambrose Street,Allen', Accepted: false, orderStatus: 'Not Accepted',
-  //   },
-  // ];
   const order = [
     {
       id: 1,
