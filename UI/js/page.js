a58de48418289f3a66aa1783c7f9a50271b7dc7c
akinyeleolat <akinyeleolat2005@gmail.com>
2018-09-20 11:06:07 +0100

    [Feature #160502620] Add users page

---
+++ UI/js/page.js
@@ -0,0 +1,53 @@
+// credit w3schools tutorial as a guide
+// modal window
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = () => {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = event => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// Toggle Tab
+
+function openOrder(evt, OrderName) {
+  let i;
+  let tabcontent;
+  let tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(OrderName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Toggle Side Panel
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
