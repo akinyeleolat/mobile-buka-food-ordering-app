503c07fccc6a7fecbeb9e87193f9713272549ae0
akinyeleolat <akinyeleolat2005@gmail.com>
2018-09-27 04:39:55 +0100

    [Chore 160705868] set up test databse

---
+++ table.sql
@@ -0,0 +1,93 @@
+DROP DATABASE IF EXISTS wmgztcls;
+CREATE DATABASE wmgztcls;
+
+\c wmgztcls;
+
+DROP TABLE IF EXISTS Item
+CASCADE;
+
+DROP TABLE IF EXISTS Users
+CASCADE;
+
+DROP TABEL
+IF EXISTS Orders CASCADE;
+
+CREATE TABLE
+IF NOT EXISTS Item
+(
+  id SERIAL PRIMARY KEY,
+
+  itemName VARCHAR
+(255) UNIQUE NOT NULL,
+
+  itemPrice FLOAT NOT NULL,
+  imageUrl TEXT NOT NULL,
+
+  menu VARCHAR
+(255) NOT NULL,
+
+  createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+  );
+
+CREATE TABLE
+IF NOT EXISTS Users
+(
+
+  id SERIAL PRIMARY KEY,
+
+  fullname VARCHAR
+(150) NOT NULL,
+
+  deliveryAddress VARCHAR
+(150) NOT NULL,
+
+  username VARCHAR
+(100) UNIQUE NOT NULL,
+
+  userType VARCHAR
+(50) NOT NULL,
+
+  email VARCHAR
+(255) UNIQUE NOT NULL,
+
+  phoneNumber VARCHAR
+(255) UNIQUE NOT NULL,
+
+  userpassword VARCHAR
+(255) NOT NULL,
+
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE
+IF NOT EXISTS Orders
+(
+  id SERIAL PRIMARY KEY,
+
+  userId int REFERENCES Users
+(id),
+
+  itemName VARCHAR
+(255) UNIQUE NOT NULL,
+
+  itemPrice FLOAT NOT NULL,
+
+  menu VARCHAR
+(255) NOT NULL,
+
+  quantity FLOAT  NOT NULL,
+
+  imageUrl TEXT NOT NULL,
+
+  amountDue  FLOAT NOT NULL,
+
+  delivery VARCHAR
+(150)  NOT NULL,
+
+  orderStatus VARCHAR
+(255) NOT NULL,
+
+  createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+  );
